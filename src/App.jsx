accbbdd9da7079abeda2b119a6efc6e975075ecf
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2021-05-26 18:18:18 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Button variant="contained" color="primary">test</Button>
+
       {console.log(process.env)}
       {console.log(window.envConfig.API_URL)}
     </div>
